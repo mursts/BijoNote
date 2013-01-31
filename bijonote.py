856af48123b1669d01ef48b309ac50686dfb15df
mursts <mursts@gmail.com>
2013-02-01 01:05:45 +0900

    pep8のスタイルに合わせた

--- bijonote.py
+++ bijonote.py
@@ -18,6 +18,7 @@ from datetime import date
 
 BASE_URL = 'http://www.bijogoyomi.com'
 
+
 def get_note_store():
     userStoreHttpClient = THttpClient.THttpClient(config.user_store_uri)
     userStoreProtocol = TBinaryProtocol.TBinaryProtocol(userStoreHttpClient)
@@ -27,11 +28,15 @@ def get_note_store():
     noteStoreProtocol = TBinaryProtocol.TBinaryProtocol(noteStoreHttpClient)
     return NoteStore.Client(noteStoreProtocol)
 
+
 def get_image_url(date):
     matcher = re.compile('<img height="517" src="([^"]*)"')
-    url = '{0}{1}{2}'.format(BASE_URL, '/bijo3/index.php/', date.strftime('%Y/%m/%d'))
+    url = '{0}{1}{2}'.format(
+        BASE_URL, '/bijo3/index.php/', date.strftime('%Y/%m/%d'))
     contents = requests.get(url).content
-    return ['{0}{1}'.format(BASE_URL, url) for url in matcher.findall(contents)]
+    return ['{0}{1}'.format(BASE_URL, url)
+            for url in matcher.findall(contents)]
+
 
 def get_image(url):
     im = Image.open(StringIO(requests.get(url).content))
@@ -41,10 +46,11 @@ def get_image(url):
     im.save(f, 'JPEG', quality=80)
     return f.getvalue()
 
+
 def create_note(date=date.today()):
     note = Types.Note()
     note.title = date.strftime('%Y-%m-%d')
-    
+
     resources = []
     hash_list = []
     for url in get_image_url(date):
@@ -52,32 +58,33 @@ def create_note(date=date.today()):
         md5 = hashlib.md5()
         md5.update(image)
         hash = md5.digest()
-        
+
         data = Types.Data()
         data.size = len(image)
         data.bodyHash = hash
         data.body = image
-        
+
         resource = Types.Resource()
         resource.mime = 'image/jpeg'
         resource.data = data
-    
+
         resources.append(resource)
         hash_list.append(hash)
-    
+
     if len(hash_list) < 1:
         return
 
     note.resources = resources
-    
+
     note.content = '<?xml version="1.0" encoding="UTF-8"?>'
     note.content += '<!DOCTYPE en-note SYSTEM ' \
         '"http://xml.evernote.com/pub/enml2.dtd">'
     note.content += '<en-note>'
     for x in hash_list:
-        note.content += '<en-media type="image/jpeg" hash="' + binascii.hexlify(x) + '"/>'
+        note.content += '<en-media type="image/jpeg" hash="' + \
+            binascii.hexlify(x) + '"/>'
     note.content += '</en-note>'
-    
+
     note_store = get_note_store()
     note_store.createNote(config.auth_token, note)
 
